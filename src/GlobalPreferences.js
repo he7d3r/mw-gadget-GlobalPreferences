d3e842c78d685a5674a15fa7510ae6ace8dbe465
Helder <he7d3r@gmail.com>
2014-02-14 12:58:49 +0000

    Changing the approach
    * Set my common preferences in the first time I visit some wiki
    * Set "userjs-already-set-common-preferences" to "1" after setting the global preferences
    * Ignore Portuguese wikis

--- src/GlobalPreferences.js
+++ src/GlobalPreferences.js
@@ -1,53 +1,41 @@
 /**
- * Changes my preferences on several wikis
+ * Set my common preferences in the first time I visit some wiki
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/changeMyPrefs.js]] ([[File:User:Helder.wiki/Tools/changeMyPrefs.js]])
  */
-/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, devel: true, maxlen: 120, evil: true, onevar: true */
-/*global mediaWiki */
-( function ( mw ) {
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
 'use strict';
 
-var wikis = [ '//ja.wikipedia.org', '//xal.wikipedia.org' ];
-
-function changeMyPrefsOn( server ) {
-	var api = new mw.Api( {
-		ajax: {
-			url: server + '/w/api.php',
-			dataType: 'jsonp'
-		}
-	} );
-
+function setPrefs() {
+	var api = new mw.Api();
 	api.get( {
 		action: 'tokens',
 		type: 'options'
 	} )
 	.done( function ( data ) {
-		console.log( data );
 		api.post( {
 			action: 'options',
 			token: data.tokens.optionstoken,
-			change: 'language=en|fancysig=1',
+			change: 'language=en|fancysig=1|userjs-already-set-common-preferences=1',
 			optionname: 'nickname',
 			optionvalue: '[[b:pt:User:' + mw.config.get('wgUserName') + '|' + mw.config.get('wgUserName') + ']]'
 		} )
-		.done( function ( data ) {
-			console.log( data );
-		} )
-		.fail( function ( data ) {
-			console.log( data );
+		.done( function () {
+			mw.notify( 'Your "global" preferences were copied to this wiki.', { autoHide: true } );
 		} );
-	} )
-	.fail( function ( data ) {
-		console.log( data );
 	} );
 }
 
-mw.loader.using( 'mediawiki.api', function(){
-	var i;
-	for ( i = 0; i < wikis.length; i++) {
-		changeMyPrefsOn( wikis[i] );
+mw.loader.using( 'user.options', function(){
+	if( !mw.user.options.get( 'userjs-already-set-common-preferences' ) &&
+		mw.config.get( 'wgContentLanguage' ) !== 'pt'
+	){
+		mw.loader.using( [ 'mediawiki.api', 'mediawiki.notify' ], function(){
+			$( setPrefs );
+		} );
 	}
 } );
 
-}( mediaWiki ) );
+}( mediaWiki, jQuery ) );

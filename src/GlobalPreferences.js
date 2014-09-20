22b34465b2ec5e93e75697756f08b8f0f517e16e
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/GlobalPreferences.js
+++ src/GlobalPreferences.js
@@ -3,7 +3,6 @@
  * 
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/GlobalPreferences.js]] ([[File:User:Helder.wiki/Tools/GlobalPreferences.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: false, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
